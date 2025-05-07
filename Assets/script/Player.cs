4b223b31ccd8acf3785f0a0aef88edda81c90588
Dawnftcd <kuhunglov3@gmail.com>
2025-05-07 21:11:14 +0700

    update dash ver 1

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -7,23 +7,24 @@ public class Player : MonoBehaviour
 
     [SerializeField] float xInput;
     [SerializeField] Rigidbody2D rb;
-
-    [SerializeField] float sPeed  ;
-    [SerializeField] float jUmpForce;
-
+    [SerializeField] float speed ;
+    [SerializeField] float jumpForce;
     [SerializeField] Animator anim;
     [SerializeField] bool isMoving;
-    
     [SerializeField] int facingDir=1;
-
     [SerializeField] bool facingRight=true;
-  [SerializeField] float groundCheckDistance;
 
-  [SerializeField ] LayerMask WhatIsGround;
+    [Header ("Dash Info")]
+    [SerializeField] float dashTime;
+    [SerializeField] float dashDuration;
+    [SerializeField] float dashSpeed;
 
-  [SerializeField ] bool isGrounded;
+    [Header  ("Collision Info") ]
+    [SerializeField] float groundCheckDistance;
+    [SerializeField ] LayerMask WhatIsGround;
+    [SerializeField ] bool isGrounded;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,12 +34,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       dashTime -=Time.deltaTime;
+
+       
+      
+
        CheckInput();
        FlipController();
        Movement();  
        AnimatorController();
        CollisionChecks();
-
     }
      
 
@@ -55,23 +60,35 @@ public class Player : MonoBehaviour
         {
            Jump();
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+       {
+            dashTime= dashDuration;
+       }
      }
      public void Movement()
      {
-         rb.linearVelocity = new Vector2  (xInput*sPeed, rb.linearVelocity.y);
+         if (dashTime > 0) 
+       {
+         rb.linearVelocity= new Vector2 (xInput*dashSpeed, 0);
+       }
+       else
+         rb.linearVelocity = new Vector2  (xInput*speed, rb.linearVelocity.y);
        
      }
     public void Jump()
     {    
         if (isGrounded)
-         rb.linearVelocity= new Vector2 (rb.linearVelocity.x, jUmpForce );
+         rb.linearVelocity= new Vector2 (rb.linearVelocity.x, jumpForce );
     }
 
     public void AnimatorController()
     {
-        isMoving = rb.linearVelocity.x !=0;
-
+        isMoving = rb.linearVelocity.x !=0; // bool
+        anim.SetFloat("Yvelocity", rb.velocity.y);
         anim.SetBool ("Ismoving", isMoving);
+        anim.SetBool ("Grounded", isGrounded);
+        anim.SetBool ("isDashing", dashTime >0);
     }
     public void Flip()
     {    facingDir = facingDir *-1; // cần dòng nay để thiết lập các thông số kỹ năng khác
@@ -81,11 +98,11 @@ public class Player : MonoBehaviour
 
     public void FlipController() 
     {
-        if (rb.linearVelocity.x  > 0 && facingRight)
+        if (rb.linearVelocity.x  > 0 && !facingRight)
         {
             Flip(); // char quay phải nhưng mặt char không phải bên phải thì lật cho !facingright thành bên phải
         }
-        else if (rb.linearVelocity.x < 0 && !facingRight)
+        else if (rb.linearVelocity.x < 0 && facingRight)
         {
             Flip();
         }
